72641b17fa637a19a344b582febbfcfde573ca73
Juan Vazquez <juanvme123@gmail.com>
2016-12-30 23:01:39 -0800

    Added example endpoints

--- index.js
+++ index.js
@@ -4,10 +4,45 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+// Prettifies json response body using 2 spaces for indentation
+app.set('json spaces', 2)
+
+// Respond to `GET` requests made to the "https://my-project-name.heroku.com/"
+app.get('/', function(req, res) {
+    var responseObject = {
+        foo: 'bar',
+        number: 42
+    };
+    res.json(responseObject);
+})
+
+// Respond to `POST` requests made to the "https://my-project-name.heroku.com/some-order"
+app.post('/some-order', function(req, res) {
+    var responseObject = {
+    	createdAt: new Date().toISOString(),
+        id: 12345,
+        firstName: 'Newt',
+        lastName: 'Scamander',
+        orderLineItems: [{
+            name: 'Hufflepuff Robes',
+            quantity: 1,
+            price: 738.75,
+            sku: 'sdf652s1d'
+        }, {
+            name: 'Barn Owl',
+            quantity: 1,
+            price: 12000.55,
+            sku: 'kj54t9e'
+        }, {
+            name: 'Parchment',
+            quantity: 1,
+            price: 6.75,
+            sku: 'pdfnk3'
+        }]
+    };
+    res.json(responseObject);
 })
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+    console.log("Node app is running at localhost:" + app.get('port'))
 })
